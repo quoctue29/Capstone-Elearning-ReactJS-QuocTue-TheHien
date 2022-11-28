8089ff76ca9c4977a3dbea81fe7d9cd76068d39d
quoctue_ <87956563+quoctue29@users.noreply.github.com>
2022-11-28 20:39:23 +0700

    home danh muc

--- src/pages/pageHome/listKhoaHoc/ListKhoaHoc.jsx
+++ src/pages/pageHome/listKhoaHoc/ListKhoaHoc.jsx
@@ -3,75 +3,94 @@ import { Divider, Tabs } from "antd";
 //import KhoaHoc from "./KhoaHoc/KhoaHoc";
 import { useDispatch } from "react-redux";
 import { useQuanLyKhoaHoc } from "../../../stores/quanLyKhoaHocReducer/quanLyPhimSelector";
-import {
-  getDanhMucKhoaHoc, getKhoaHocList,
-  // getKhoaHocList,
-  // getKhoaHocTheoDanhMuc,
-} from "../../../stores/quanLyKhoaHocReducer/quanLyKhoaHocReducer";
+import { getKhoaHocList } from "../../../stores/quanLyKhoaHocReducer/quanLyKhoaHocReducer";
 import { useEffect } from "react";
-//import TabPane from "antd/lib/tabs/TabPane";
-//import KhoaHocTheoDanhMuc from "./KhoaHocTheoDanhMuc/KhoaHocTheoDanhMuc";
 import { useState } from "react";
 import { quanLyCourseServices } from "../../../services/quanLyCourseServices";
 import { useNavigate } from "react-router-dom";
-
+import "./card.css";
+import { Col, Row } from "antd";
 
 export default function ListKhoaHoc() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [filter, setFilter] = useState({ maDanhMuc: "BackEnd" });
-  const [coures, setCoures] = useState([])
+  const [coures, setCoures] = useState([]);
   const { listKhoaHoc } = useQuanLyKhoaHoc();
   const { listDanhMuc } = useQuanLyKhoaHoc();
+  const style = {
+    padding: "8px 0"
+  };
   console.log(listDanhMuc);
   useEffect(() => {
     (async () => {
       const res = await quanLyCourseServices.getKhoaHocTheoDanhMuc(filter);
-      setCoures(res.data)
+      setCoures(res.data);
       console.log(res);
     })();
   }, [filter]);
-  useEffect(() =>{
-    dispatch(getKhoaHocList())
-  },[])
+  useEffect(() => {
+    dispatch(getKhoaHocList());
+  }, []);
   const handleChange = async (activeKey) => {
     setFilter({ ...filter, maDanhMuc: activeKey });
   };
+
   return (
     <div className="container" id="khoahoc">
       <Tabs defaultActiveKey="BackEnd" onChange={handleChange}>
         {listDanhMuc.length > 0 &&
           listDanhMuc.map((category, index) => (
             <Tabs.TabPane tab={category.tenDanhMuc} key={category.maDanhMuc}>
-                <div>
-
-                {coures.map((khoaHoc, i) => (
-                  <div key={i}>
-                    <div className="p-5">
-                      <div className="shadow-lg border">
-                        <div className="overflow-hidden">
-                          <img
-                            src={khoaHoc.hinhAnh}
-                            alt=""
-                            className="w-full h-[350px] "
-                            title={khoaHoc.moTa}
-                          />
-                        </div>
-                        <div className="flex flex-col justify-between">
-                          <button
-                            onClick={() =>
-                              navigate(`/detail/${khoaHoc.maKhoaHoc}`)
-                            }
-                            className="w-full font-bold transition duration-300 mb-0 bg-yellow-500 text-lg p-1  shadow hover:bg-red-600 "
-                          >
-                            Đăng ký
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-                </div>
+              <Row gutter={{
+                        xs: 8,
+                        sm: 16,
+                        md: 24,
+                        lg: 32
+                      }}>
+                    {coures.map((khoaHoc, i) => (
+                        <Col className="gutter-row" span={6} key={i}>
+                          <div className="card">
+                            <div className="overflow-hidden">
+                              <img
+                                src={khoaHoc.hinhAnh}
+                                alt=""
+                                className="w-full h-[150px] "
+                                title={khoaHoc.moTa}
+                              />
+                            </div>
+                            <div class="card-body">
+                              <h5 class="card-title">{khoaHoc.tenKhoaHoc}</h5>
+                              <div class="card__star">
+                                <span>
+                                  <i class="fa fa-star"></i>
+                                  <i class="fa fa-star"></i>
+                                  <i class="fa fa-star"></i>
+                                  <i class="fa fa-star"></i>
+                                  <i class="fa fa-star-half"></i>
+                                </span>
+                                <span class="card__rate">
+                                  {khoaHoc.danhGia}
+                                </span>
+                                <span class="card__total">
+                                  ({khoaHoc.luotXem})
+                                </span>
+                              </div>
+                            </div>
+                            <div className="flex flex-col justify-between">
+                              <button
+                                onClick={() =>
+                                  navigate(`/detail/${khoaHoc.maKhoaHoc}`)
+                                }
+                                className="w-full font-bold transition duration-300 mb-0 bg-yellow-500 text-lg p-1  shadow hover:bg-red-600 "
+                              >
+                                Xem chi tiết
+                              </button>
+                            </div>
+                          </div>
+                        </Col>
+                    ))}
+                      </Row>
             </Tabs.TabPane>
           ))}
       </Tabs>
